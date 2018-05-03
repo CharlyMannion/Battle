3def082c54fc0b7b28d55f771fae4fcb92114634
Charly Mannion <charlottemannion100@gmail.com>
2018-05-03 14:28:05 +0100

    added player 1 to game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,11 @@
 class Game
 
+  attr_reader :player_1
+
+  def initialize(player_1 = Player.new)
+    @player_1 = player_1
+  end
+
   def attack(player)
     player.receive_damage
   end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,9 +1,16 @@
 require 'game'
 
 describe Game do
-  subject(:game) { described_class.new }
+  subject(:game) { described_class.new(:roxana) }
+  let(:roxana) { Player.new(:name_roxana) }
   let(:zoe) { double :player }
   let(:charly) { double :player }
+
+  describe '#initalize' do
+    it 'should have a player 1' do
+      expect(game.player_1).to eq :roxana
+    end
+  end
 
   describe '#attack' do
     it 'damages the other player' do
